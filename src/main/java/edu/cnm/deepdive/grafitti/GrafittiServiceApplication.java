ef8804a29ec45f9ae439c7e12730072832a74cc8
sam morales <smorales40@cnm.edu>
2023-10-31 13:23:13 -0600

    Initial commit.

---
+++ src/main/java/edu/cnm/deepdive/grafitti/GrafittiServiceApplication.java
@@ -0,0 +1,13 @@
+package edu.cnm.deepdive.grafitti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrafittiServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GrafittiServiceApplication.class, args);
+  }
+
+}
---
+++ src/test/java/edu/cnm/deepdive/grafitti/GrafittiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cnm.deepdive.grafitti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrafittiServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
