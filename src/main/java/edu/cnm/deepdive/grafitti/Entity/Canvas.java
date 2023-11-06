576d32ca358910eb8457b17267549e7cdcf1a844
che money <cmelendez15@cnm.edu>
2023-11-06 13:47:29 -0600

    started documentation

--- src/main/java/edu/cnm/deepdive/grafitti/Entity/Canvas.java
+++ src/main/java/edu/cnm/deepdive/grafitti/Entity/Canvas.java
@@ -1,6 +1,18 @@
 package edu.cnm.deepdive.grafitti.Entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 
+@Entity
 public class Canvas {
+  @Id
+  @GeneratedValue
+  @Column(name = "canvas_id", updatable = false)
+  private Long id;
 
+  public Long getId() {
+    return id;
+  }
 }
