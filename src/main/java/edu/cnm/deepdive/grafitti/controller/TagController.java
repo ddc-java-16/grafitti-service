e24ec54c1f87313f670d978bcd7f5882ea04d5db
che money <cmelendez15@cnm.edu>
2023-11-14 22:04:47 -0600

    starting service layer

---
+++ src/main/java/edu/cnm/deepdive/grafitti/controller/TagController.java
@@ -0,0 +1,5 @@
+package edu.cnm.deepdive.grafitti.controller;
+@RestController
+public class TagController {
+
+}
---
+++ src/main/java/edu/cnm/deepdive/grafitti/service/StorageService.java
@@ -0,0 +1,51 @@
+package edu.cnm.deepdive.grafitti.service;
+
+import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
+
+public interface StorageService {
+
+String store(MultipartFile file) throws StorageException, MediaTypeException;
+
+Resource retrieve(String key) throws StorageException;
+
+boolean delete(String key)  throws StorageException, UnsupportedOperationException, SecurityException;
+
+
+class MediaTypeException extends RuntimeException{
+
+  public MediaTypeException() {
+  }
+
+  public MediaTypeException(String message) {
+    super(message);
+  }
+
+  public MediaTypeException(String message, Throwable cause) {
+    super(message, cause);
+  }
+
+  public MediaTypeException(Throwable cause) {
+    super(cause);
+  }
+}
+
+class StorageException extends RuntimeException{
+
+    public StorageException() {
+    }
+
+    public StorageException(String message) {
+      super(message);
+    }
+
+    public StorageException(String message, Throwable cause) {
+      super(message, cause);
+    }
+
+    public StorageException(Throwable cause) {
+      super(cause);
+    }
+  }
+
+}
